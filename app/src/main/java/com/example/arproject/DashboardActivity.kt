5e2f31e3b26bbe4c801057ae74e63fdae0e3a02f
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-19 13:51:28 +0530

    added view binding

--- app/src/main/java/com/example/arproject/DashboardActivity.kt
+++ app/src/main/java/com/example/arproject/DashboardActivity.kt
@@ -2,10 +2,13 @@ package com.example.arproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.arproject.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
