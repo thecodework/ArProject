277be5f979f431160602c9a60c046c265db7a209
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-19 14:59:02 +0530

    added fragment in bottom nav

--- app/src/main/java/com/example/arproject/DashboardActivity.kt
+++ app/src/main/java/com/example/arproject/DashboardActivity.kt
@@ -2,6 +2,7 @@ package com.example.arproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.arproject.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
@@ -10,5 +11,30 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializer()
+        setListener()
     }
+
+    private fun initializer() {
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, HomeFragment())
+            .commit()
+    }
+
+    private fun setListener() {
+        binding.bottomNav.setOnItemSelectedListener {
+            var f: Fragment? = null
+            when (it.itemId) {
+                R.id.menuHome -> f = HomeFragment()
+                R.id.menuShop -> f = ShopFragment()
+                R.id.menuCart -> f = CartFragment()
+                R.id.menuProfile -> f = ProfileFragment()
+            }
+            if (f != null) {
+                supportFragmentManager.beginTransaction().replace(R.id.frameLayout, f).commit()
+            }
+            true
+        }
+    }
+
+
 }
