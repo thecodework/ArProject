05e3d065bbb05c27db11f0a6db399831f12b9783
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-19 18:07:19 +0530

    added rvproduct in HomeFragment

---
+++ app/src/main/java/com/example/arproject/adapter/ProductAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.arproject.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.arproject.R
+import com.example.arproject.databinding.ActivityDashboardBinding.inflate
+import com.example.arproject.databinding.RowCategoryBinding
+import com.example.arproject.databinding.RowProductBinding
+import com.example.arproject.model.ModelCategory
+
+class ProductAdapter(
+    private val context: Context?,
+    private val arraylist: ArrayList<ModelCategory>
+) :
+    RecyclerView.Adapter<ProductAdapter.myholder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myholder {
+        val binding =
+            RowProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return myholder(binding)
+    }
+
+    override fun onBindViewHolder(holder: myholder, position: Int) {
+        if (context != null) {
+            with(holder) {
+                binding.imageItem.setImageResource(arraylist[position].categoryimage)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return arraylist.size
+    }
+
+    class myholder(val binding: RowProductBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
--- app/src/main/java/com/example/arproject/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/arproject/fragment/HomeFragment.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.arproject.R
 import com.example.arproject.adapter.CategoryAdapter
+import com.example.arproject.adapter.ProductAdapter
 import com.example.arproject.databinding.FragmentHomeBinding
 import com.example.arproject.model.ModelCategory
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
     var arraylist: ArrayList<ModelCategory> = ArrayList()
+    var productlist: ArrayList<ModelCategory> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,6 +41,13 @@ class HomeFragment : Fragment() {
         arraylist.add(ModelCategory(R.drawable.bed, "Bed"))
         arraylist.add(ModelCategory(R.drawable.sofa, "Sofa"))
         arraylist.add(ModelCategory(R.drawable.bed, "Bed"))
+        productlist.add(ModelCategory(R.drawable.yellowsofa, "Sofa"))
+        productlist.add(ModelCategory(R.drawable.table, "table"))
+        productlist.add(ModelCategory(R.drawable.yellowsofa, "Sofa"))
+        productlist.add(ModelCategory(R.drawable.table, "table"))
+        productlist.add(ModelCategory(R.drawable.yellowsofa, "Sofa"))
+        productlist.add(ModelCategory(R.drawable.table, "table"))
         binding.rvCategory.adapter=CategoryAdapter(context,arraylist)
+        binding.rvProducts.adapter=ProductAdapter(context,productlist)
     }
 }
