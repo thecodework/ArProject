49d74b7834ea4818348218feeaba8f52e410e6e0
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-20 16:32:59 +0530

    added getintent in DetailsActivity

--- app/src/main/java/com/example/arproject/DetailsActivity.kt
+++ app/src/main/java/com/example/arproject/DetailsActivity.kt
@@ -2,10 +2,30 @@ package com.example.arproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.arproject.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializer()
+        setListener()
+    }
+
+    private fun setListener() {
+        binding.btnAR.setOnClickListener(View.OnClickListener {
+
+        })
+    }
+
+    private fun initializer() {
+        Utils.changeStatusBar(this, R.color.white_new)
+        val pic: Int = intent.getIntExtra("image", 0)
+        binding.imageItem.setImageResource(pic)
     }
 }
+
+
