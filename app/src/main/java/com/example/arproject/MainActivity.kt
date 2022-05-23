23339eabfe816a7e3b4bead1f32f1383f916c739
amrutaradke <amruta.p.radke@gmail.com>
2022-05-23 18:28:15 +0530

    Made changes in Ar model code

--- app/src/main/java/com/example/arproject/MainActivity.kt
+++ app/src/main/java/com/example/arproject/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.arproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Slide
-import android.transition.TransitionManager
-import android.view.Gravity
-import android.widget.Button
-import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
-import com.example.arproject.databinding.ActivityLoginBinding
 import com.example.arproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         binding.btnViewAR.setOnClickListener{
-            val intent = Intent(this, ViewARModelFragment::class.java)
+            val intent = Intent(this, ViewARModelActivity::class.java)
             startActivity(intent)
 
         }
