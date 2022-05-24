da6260dd2ff94236f104c18a9d9315cee40ddea7
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-24 19:42:14 +0530

    added field  validtation in LoginActivity

--- app/src/main/java/com/example/arproject/LoginActivity.kt
+++ app/src/main/java/com/example/arproject/LoginActivity.kt
@@ -3,23 +3,57 @@ package com.example.arproject
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.adapters.ViewGroupBindingAdapter.setListener
+import com.example.arproject.Utils.Companion.changeStatusBar
+import com.example.arproject.Utils.Companion.isValidEmail
+import com.example.arproject.Utils.Companion.isValidPassword
 import com.example.arproject.databinding.ActivityLoginBinding
 
-class LoginActivity: AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        initializer()
+        setListener()
+    }
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
-
-        binding.btnLoginActivity.setOnClickListener{
-            val intent = Intent(this, DashboardActivity::class.java)
-            startActivity(intent)
-
+    private fun setListener() {
+        binding.btnLoginActivity.setOnClickListener {
+            val password = binding.edPassword.getText().toString().trim()
+            val email = binding.edEmail.getText().toString().trim()
+            if (isValidEmail(email)) {
+                if (isValidPassword(password)) {
+                    if (password.length > 6) {
+                        val intent = Intent(this, DashboardActivity::class.java)
+                        startActivity(intent)
+                        Toast.makeText(this, "Valid", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "Password should be greater than 6 digit",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Password should be Strong",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else {
+                Toast.makeText(this, "Enter valid emailid", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun initializer() {
+
     }
 
 }
--- app/src/main/java/com/example/arproject/Utils.kt
+++ app/src/main/java/com/example/arproject/Utils.kt
@@ -1,8 +1,14 @@
 package com.example.arproject
 
+import android.R.attr
 import android.app.Activity
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 
 class Utils {
 
@@ -12,6 +18,19 @@ class Utils {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             window.statusBarColor = ContextCompat.getColor(activity, color)
+        }
+
+        fun isValidPassword(password: String?): Boolean {
+            password?.let {
+                val passwordPattern =
+                    "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+                val passwordMatcher = Regex(passwordPattern)
+
+                return passwordMatcher.find(password) != null
+            } ?: return false
+        }
+        fun isValidEmail(email: String): Boolean {
+            return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
         }
     }
 }
