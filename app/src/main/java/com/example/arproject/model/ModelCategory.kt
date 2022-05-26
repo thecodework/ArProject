8ca70487a1d341092f13d835cdbf0e9d5ab7c3c9
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-26 16:35:12 +0530

    added field in ModelCategory class

--- app/src/main/java/com/example/arproject/model/ModelCategory.kt
+++ app/src/main/java/com/example/arproject/model/ModelCategory.kt
@@ -2,14 +2,8 @@ package com.example.arproject.model
 
 data class ModelCategory(
     val categoryimage: Int,
-    val categoryname: String
-)
-
-/*
-data class ModelCategory(
-    val categoryimage: Int,
     val categoryname: String,
-    val rating: Int,
-    val price: Int,
-    val details: String
-)*/
+    val rating: Float = 4.5f,
+    val price: Int = 470,
+    val details: String="this is demo model"
+)
