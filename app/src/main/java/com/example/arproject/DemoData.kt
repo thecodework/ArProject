da9625e9da1415b37e787f7b6d8587448741e0b7
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-26 16:36:13 +0530

    added method in DemoData class

--- app/src/main/java/com/example/arproject/DemoData.kt
+++ app/src/main/java/com/example/arproject/DemoData.kt
@@ -1,13 +1,45 @@
 package com.example.arproject
 
 import com.example.arproject.model.ModelCategory
+import kotlin.random.Random
 
 class DemoData {
-
     companion object {
-        fun getDemoModel(): ModelCategory {
-            // return ModelCategory(R.drawable.sofa, "sofa",4,700,"Lorem ipsum dolor")
-            return ModelCategory(R.drawable.sofa, "sofa")
+        fun getProductList(): ArrayList<ModelCategory> {
+            return arrayListOf(
+                getDemoModel(), getDemoModel(), getDemoModel(),
+                getDemoModel(), getDemoModel(), getDemoModel()
+            )
         }
+
+        private fun getDemoModel(): ModelCategory {
+            val randomValue = Random.nextInt(1, 5)
+            return ModelCategory(
+                getProductImage(randomValue),
+                getProductName(randomValue),
+                Random.nextDouble(0.0, 5.0).toFloat(),
+                Random.nextInt(5000, 25000),
+                getProductDescription()
+            )
+        }
+
+        private fun getProductImage(pic: Int): Int {
+            val productImageMap: Map<Int, Int> = mapOf(
+                1 to R.drawable.sofa,
+                2 to R.drawable.bed,
+                3 to R.drawable.table,
+                4 to R.drawable.yellowsofa,
+                5 to R.drawable.blacksofa
+            )
+            return productImageMap.getValue(pic)
+        }
+
+        private fun getProductName(name: Int): String {
+            val productName =
+                mapOf(1 to "Sofa", 2 to "Bed", 3 to "Table", 4 to "Sofa", 5 to "Sofa")
+            return productName.getValue(name)
+        }
+
+        private fun getProductDescription(): String = "This is a piece of an Art"
     }
 }
