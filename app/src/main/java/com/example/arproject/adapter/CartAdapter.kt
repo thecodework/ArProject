fd75bf91f441ea7080b180e15532e4d5cb513802
SonamAgrahari <sonamagrahari11@gmail.com>
2022-05-31 16:56:52 +0530

    changed ui of cart.xml

--- app/src/main/java/com/example/arproject/adapter/CartAdapter.kt
+++ app/src/main/java/com/example/arproject/adapter/CartAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.arproject.R
+import com.example.arproject.databinding.RowCartBinding
 import com.example.arproject.databinding.RowShopBinding
 import com.example.arproject.model.ModelCategory
 
@@ -15,9 +16,9 @@ class CartAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding: RowShopBinding = DataBindingUtil.inflate(
+        val binding: RowCartBinding = DataBindingUtil.inflate(
             inflater,
-            R.layout.row_shop, parent, false
+            R.layout.row_cart, parent, false
         )
         return MyHolder(binding)
     }
@@ -32,7 +33,7 @@ class CartAdapter(
         return arraylist.size
     }
 
-    class MyHolder(val binding: RowShopBinding) :
+    class MyHolder(val binding: RowCartBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(modelCategory: ModelCategory) {
             binding.modelCategory = modelCategory
