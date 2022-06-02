6f8863052e0b6338834f1fa9680391684ca91cc5
SonamAgrahari <sonamagrahari11@gmail.com>
2022-06-02 17:42:43 +0530

    removed deprecated code

--- app/src/main/java/com/example/arproject/utils/Utils.kt
+++ app/src/main/java/com/example/arproject/utils/Utils.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.app.Dialog
 import android.os.Build
 import android.text.TextUtils
-import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.view.Window
@@ -21,12 +20,7 @@ class Utils {
                 view.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
             } else {
                 view.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                            View.SYSTEM_UI_FLAG_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
             }
         }
         fun isValidPassword(password: String?): Boolean {
