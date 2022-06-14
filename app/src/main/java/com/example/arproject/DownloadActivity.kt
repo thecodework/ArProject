e491d14248d99384794b8526bc6806a82b2d986b
SonamAgrahari <sonamagrahari11@gmail.com>
2022-06-14 15:11:00 +0530

    implemented file download codes

--- app/src/main/java/com/example/arproject/DownloadActivity.kt
+++ app/src/main/java/com/example/arproject/DownloadActivity.kt
@@ -1,28 +1,52 @@
 package com.example.arproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.adapters.ViewGroupBindingAdapter.setListener
 import com.example.arproject.databinding.ActivityDownloadBinding
+import okhttp3.*
+import java.io.IOException
 
 class DownloadActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
     lateinit var binding: ActivityDownloadBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_download)
-        initailizer()
+        initializer()
         setListener()
     }
 
     private fun setListener() {
-        binding.btnDownload.setOnClickListener(View.OnClickListener {
+        binding.btnDownload.setOnClickListener {
+            downloadFile()
+        }
+    }
 
+    private fun downloadFile() {
+        val request = Request.Builder()
+            .url("http://publicobject.com/helloworld.txt")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+                    println(response.body!!.string())
+                }
+            }
         })
     }
 
-    private fun initailizer() {
-        TODO("Not yet implemented")
+    private fun initializer() {
+
     }
 }
