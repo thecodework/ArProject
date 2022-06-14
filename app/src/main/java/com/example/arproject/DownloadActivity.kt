0a061c3c7168332328cdaecd6e64d0baf238b8e6
SonamAgrahari <sonamagrahari11@gmail.com>
2022-06-14 19:55:32 +0530

    save file from url in internal storage

--- app/src/main/java/com/example/arproject/DownloadActivity.kt
+++ app/src/main/java/com/example/arproject/DownloadActivity.kt
@@ -1,19 +1,24 @@
 package com.example.arproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.arproject.databinding.ActivityDownloadBinding
 import okhttp3.*
-import java.io.IOException
+import java.io.*
+
 
 class DownloadActivity : AppCompatActivity() {
     private val client = OkHttpClient()
+    private val filepath = "MyFileStorage"
+    var url: String =
+        "https://drive.google.com/u/2/uc?id=1ZrVO6LdVIZS8kfMtQCcP4LmUNk2BlINj&export=download"
+    var pdfurl: String = "https://www.hq.nasa.gov/alsj/a17/A17_FlightPlan.pdf"
     lateinit var binding: ActivityDownloadBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_download)
-        initializer()
         setListener()
     }
 
@@ -25,7 +30,7 @@ class DownloadActivity : AppCompatActivity() {
 
     private fun downloadFile() {
         val request = Request.Builder()
-            .url("http://publicobject.com/helloworld.txt")
+            .url(url)
             .build()
 
         client.newCall(request).enqueue(object : Callback {
@@ -36,17 +41,33 @@ class DownloadActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 response.use {
                     if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                    for ((name, value) in response.headers) {
-                        println("$name: $value")
+                    val result = response.body?.byteStream()
+                    result?.let {
+                        writeResponse(it)
                     }
-                    println(response.body!!.string())
                 }
             }
         })
     }
 
-    private fun initializer() {
-
+    fun writeResponse(inputStream: InputStream) {
+        try {
+            val fileReader = ByteArray(4096)
+            var fileSize = 0
+            val myExternalFile = File(getExternalFilesDir(filepath), "model.glb")
+            val fileOutPutStream = FileOutputStream(myExternalFile)
+            val fos: OutputStream = fileOutPutStream
+            do {
+                val read = inputStream.read(fileReader)
+                if (read != -1) {
+                    fos.write(fileReader, 0, read)
+                    fileSize += read
+                }
+            } while (read != -1)
+            fos.flush()
+            fos.close()
+        } catch (e: IOException) {
+            Log.d("check", e.message.toString())
+        }
     }
 }
